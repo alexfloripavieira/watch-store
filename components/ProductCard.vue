7537183df2385bd827bab1e92947550f15c29cee
Alexsander Vieira <alexsander.vieira@neoway.com.br>
2022-12-21 20:15:44 -0300

    created store

---
+++ components/ProductCard.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div.product-card__container
+    p
+      | watch-store
+</template>
+
+<script>
+export default {
+  name: 'NuxtProductCard',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  product-card
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
